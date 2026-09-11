7787042bf27f3283ce2443688f5bdacfd465f013
River Marks <rmarks6767@gmail.com>
2026-09-11 16:16:02 -0700

    Boilerplate added

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export const App = () => {
+  // Things go here probably
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to React</h1>
+      </header>
+    </div>
+  );
+};
